5f6ab4ff1f8371fc60d867072d0cf1551cc519a5
danielkocjan <daniel.kocjan@skygate.io>
2019-07-20 17:59:30 +0200

    Add user mocks based on github api

---
+++ src/modules/user/__mocks__/userDetailsMocks.ts
@@ -0,0 +1,20 @@
+import { UserDetails } from '../models/userModels';
+
+export const userDetailsMock: UserDetails = {
+    login: 'danielkocjan',
+    id: 21957456,
+    avatarUrl: 'https://i.pravatar.cc/400?u=21957456',
+    url: 'https://api.github.com/users/danielkocjan',
+    htmlUrl: 'https://github.com/danielkocjan',
+    reposUrl: 'https://api.github.com/users/danielkocjan/repos',
+    name: 'Daniel Kocjan',
+    company: '@skygate ',
+    location: 'Kraków, Poland',
+    email: 'dkocjan@pm.me',
+    blog: 'https://linkedin.com/in/dkocjan',
+    hireable: true,
+    bio: 'JS Developer',
+    publicRepos: 14,
+    followers: 5,
+    following: 4,
+};
---
+++ src/modules/user/__mocks__/usersMocks.ts
@@ -0,0 +1,36 @@
+import { User } from '../models/userModels';
+
+export const usersMocks: User[] = [
+    {
+        login: 'mojombo',
+        id: 1,
+        avatarUrl: 'https://i.pravatar.cc/400',
+        url: 'https://api.github.com/users/mojombo',
+        htmlUrl: 'https://github.com/mojombo',
+        reposUrl: 'https://api.github.com/users/mojombo/repos',
+    },
+    {
+        login: 'defunkt',
+        id: 2,
+        avatarUrl: 'https://i.pravatar.cc/401',
+        url: 'https://api.github.com/users/defunkt',
+        htmlUrl: 'https://github.com/defunkt',
+        reposUrl: 'https://api.github.com/users/defunkt/repos',
+    },
+    {
+        login: 'pjhyett',
+        id: 3,
+        avatarUrl: 'https://i.pravatar.cc/402',
+        url: 'https://api.github.com/users/pjhyett',
+        htmlUrl: 'https://github.com/pjhyett',
+        reposUrl: 'https://api.github.com/users/pjhyett/repos',
+    },
+    {
+        login: 'danielkocjan',
+        id: 21957456,
+        avatarUrl: 'https://i.pravatar.cc/403',
+        url: 'https://api.github.com/users/danielkocjan',
+        htmlUrl: 'https://github.com/danielkocjan',
+        reposUrl: 'https://api.github.com/users/danielkocjan/repos',
+    },
+];
